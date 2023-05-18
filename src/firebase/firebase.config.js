8a3f526ea792600cd2343d5e6a93609cbff84811
noornabi07 <noornabiprogram07@gmail.com>
2023-05-18 16:08:49 +0600

    firebase keys hide by local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTaSiQZ5vODSjoTddxGGb4hWRXTzjSe5Q",
-  authDomain: "animal-toys-shop.firebaseapp.com",
-  projectId: "animal-toys-shop",
-  storageBucket: "animal-toys-shop.appspot.com",
-  messagingSenderId: "56085511171",
-  appId: "1:56085511171:web:4569134eac7db37a73e236"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
