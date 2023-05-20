ed2235f46c8bb4d21b5eac4ca6f13c4b1b3f933e
noornabi07 <noornabiprogram07@gmail.com>
2023-05-20 15:06:52 +0600

    footer modify design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
 import logo from '../../../public/animal-2.png'
-import { FaBeer, FaFacebook, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaBeer, FaFacebook, FaGithub, FaGoogle, FaInstagram, FaLocationArrow, FaMap, FaMapMarked, FaMapMarkedAlt, FaMobile, FaPhoneSquare, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <div>
             <footer className="footer p-10 bg-teal-600 text-white">
                 <div>
-                   <img className='w-40 rounded-full' src={logo} alt="" />
-                    <p className='font-semibold'>Animal Toys Shop.<br />Providing reliable tech since 1992</p>
+                    <img className='w-40 rounded-full' src={logo} alt="" />
+                    <p className='font-semibold'>Animal Toys Shop.<br />Providing reliable tech since 2023</p>
+                </div>
+                <div className='text-lg'>
+                    <span className="footer-title text-2xl">Services</span>
+                    <a className="link link-hover hover:ml-2 hover:duration-200 hover:text-red-600">Animal Toys</a>
+                    <a className="link link-hover hover:ml-2 hover:duration-200 hover:text-red-600">Kids Toys</a>
+                    <a className="link link-hover hover:ml-2 hover:duration-200 hover:text-red-600">Learning Toys</a>
+                    <a className="link link-hover hover:ml-2 hover:duration-200 hover:text-red-600">Musical Toys</a>
+                </div>
+                <div className='text-lg'>
+                    <span className="footer-title text-2xl"> Contact</span>
+                    <a className="link link-hover flex items-center">
+                        <FaPhoneSquare className='text-xl text-white font-bold mr-3'></FaPhoneSquare>
+                        <span> 01826147180</span>
+                    </a>
+
+                    <a className="link link-hover flex items-center">
+                        <FaGoogle className='mr-3 text-xl font-bold'></FaGoogle>
+                        noornabiprogram07@gmail.com
+                    </a>
+
+                    <a className="link link-hover flex items-center">
+                        <FaLocationArrow className='font-bold text-xl mr-3'></FaLocationArrow>
+                        Dinajpur
+                    </a>
+
+                    <a className="link link-hover flex items-center">
+                        <p>@copyright all the reserved by noornabi</p>
+                    </a>
+
+
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Animal Toys</a>
-                    <a className="link link-hover">Kids Toys</a>
-                    <a className="link link-hover">Learning Toys</a>
-                    <a className="link link-hover">Musical Toys</a>
-                </div>
-                <div>
-                    <span className="footer-title">Contact</span>
-                    <a className="link link-hover">Mobile: 01826147180</a>
-                    <a className="link link-hover">Email: noornabiprogram07@gmail.com</a>
-                    <a className="link link-hover">Address: Dinajpur</a>
-                    <a className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
+                    <span className="footer-title text-2xl">Legal</span>
                     <a className="link link-hover" href='https://www.facebook.com/mdnoornabiislamNK' target='_blank'>
                         <FaFacebook className='text-3xl hover:rotate-45 hover:duration-500'></FaFacebook>
                     </a>
