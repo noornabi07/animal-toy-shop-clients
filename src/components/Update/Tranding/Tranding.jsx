84ad4e047b47fa733a2f632f97f649871b527a61
noornabi07 <noornabiprogram07@gmail.com>
2023-05-21 19:44:04 +0600

    make modify tranding section

--- src/components/Update/Tranding/Tranding.jsx
+++ src/components/Update/Tranding/Tranding.jsx
@@ -11,7 +11,7 @@ const Tranding = () => {
         <div className='px-5'>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-10' data-aos="fade-left"
                 data-aos-easing="linear"
-                data-aos-duration="1000">
+                data-aos-duration="1500">
                 <img className='w-40 mx-auto h-32 p-4 rounded-full' src={logo1} alt="" />
                 <img className='w-40 mx-auto h-32 p-4 rounded-full' src={logo2} alt="" />
                 <img className='w-40 mx-auto h-32 p-4 rounded-full' src={logo3} alt="" />
